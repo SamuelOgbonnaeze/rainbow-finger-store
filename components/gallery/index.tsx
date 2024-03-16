76457e112784db15422f515fdeca1263e0838ac2
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-03-16 12:38:32 +0100

    Gallery and Gallery Tab added to individual product page

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,17 +1,41 @@
 "use client"
 
-import {Tab} from "@headlessui/react"
-import { Image } from "@/types";
+import { Tab } from "@headlessui/react"
+import Image from "next/image"
+
+import { Image as ImageType } from "@/types";
+
+import GalleryTab from "./gallery-tab";
 
 interface GalleryProps {
-    images: Image[];
+    images: ImageType[];
 }
 
-const Gallery = () => {
+const Gallery: React.FC<GalleryProps> = ({ images }) => {
     return (
-        <div>
-            kjd
-        </div>
+        <Tab.Group as="div" className="flex flex-col-reverse">
+            <div className="mx-auto mt-6 hidden w-full max-2xl sm:block lg:max-w-none">
+                <Tab.List className="grid grid-cols-4 gap-6">
+                    {images.map((image) => (
+                        <GalleryTab key={image.id} image={image} />
+                    ))}
+                </Tab.List>
+            </div>
+            <Tab.Panels className="aspect-square w-full">
+                {images.map((image) => (
+                    <Tab.Panel key={image.id}>
+                        <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+                            <Image
+                                fill
+                                src={image.url}
+                                alt="Product Image"
+                                className="object-cover object-center"
+                            />
+                        </div>
+                    </Tab.Panel>
+                ))}
+            </Tab.Panels>
+        </Tab.Group>
     )
 }
 
