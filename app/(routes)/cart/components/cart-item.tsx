d1d5fbeea863ad8719a6acd5d317e66c20493dad
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-03-30 17:56:06 +0100

    Cart page added

---
+++ app/(routes)/cart/components/cart-item.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import { X } from "lucide-react";
+
+import { Product } from "@/types";
+import IconButton from "@/components/ui/icon-button";
+
+interface CartItemProps {
+    data: Product;
+}
+
+const CartItem: React.FC<CartItemProps> = ({ data }) => {
+    return (
+        <li className="flex py-6 border-b">
+            <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
+                <Image
+                    fill
+                    src={data.images[0].url}
+                    alt="Product Image"
+                    className="object-cover object-center"
+                />
+            </div>
+            <div className="relative ml-4 flex flex-1 flex-col  justify-between sm:ml-6">
+                <div className="absolute z-10 right-0 top-0">
+                    <IconButton onClick={() => { }} icon={<X size={15} />} />
+                </div>
+                <div className="sm:py-8 items-center">
+                    <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-2 sm:pr-0">
+                        <div className="flex justify-between">
+                            <p className="text-lg font-semibold text-black">
+                                {data.name}
+                            </p>
+                        </div>
+                        <div className="mt-1 flex text-sm">
+                            <p className="text-gray-500">{data.brand.name}</p>
+                            <p className="text-gray-500 ml-2 border-l border-gray-300 pl-4">{data.color.name}</p>
+                            <p className="text-gray-500 ml-2 border-l border-gray-300 pl-4">{data.size.name}</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </li>
+    )
+}
+
+export default CartItem;
--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import Container from "@/components/ui/container";
+import useCart from "@/hooks/use-cart";
 import { useEffect, useState } from "react";
+import CartItem from "./components/cart-item";
 
 
 const CartPage = () => {
@@ -10,15 +13,32 @@ const CartPage = () => {
         setIsMounted(true)
     }, [])
 
+    const cart = useCart();
+
     if (!isMounted) {
         return null;
     }
 
     return (
-        <div>
-
-
-            Cart Page
+        <div className="bg-white">
+            <Container>
+                <div className="px-4 py-16 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold">Your shopping cart</h1>
+                    <div className=" mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+                        <div className="lg:col-span-7">
+                            {cart.items.length === 0 && <p className="text-neutral-500">There are no items currently in your cart </p>}
+                            <ul>
+                                {cart.items.map((item) => (
+                                    <CartItem
+                                        key={item.id}
+                                        data={item}
+                                    />
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 }
