abc0ea202831a00c3eb8e21e338d5ef8018b6680
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-05-01 13:55:01 +0100

    minor commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,12 +3,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { BsSearch } from 'react-icons/bs';
 import HeroImage from '../public/images/hero-image-1.png';
+import { ShoppingCart } from 'lucide-react';
 
-interface HeroLinkProps {
+interface HeroTextProps {
     text: string
 }
 
-const HeroLink: React.FC<HeroLinkProps> = ({ text }) => {
+const HeroText: React.FC<HeroTextProps> = ({ text }) => {
     return (
         <div className='w-[100px] flex items-center md:w-[308px] h-[105px] lg:h-[112px] text-center rounded-[25px] border-[1px] border-[#FCEBE7] bg-[#600000]'>
             <p className='w-[80%] mx-auto lg:leading-[64px] text-[#FBFBFB] font-light font-nunito'>{text}</p>
@@ -25,10 +26,14 @@ const LeftSection = () => {
 
             </div>
             <div className='flex justify-between mb-6'>
-               
+
                 <Link href="/product">
-                    <div className='flex w-[200px] lg:w-[295px] lg:h-[48px] justify-normal'>
-                        <button className='w-full h-full bg-[#E24F29] px-3 text-white rounded-[60px] gap-[10px] py-2 lg:py-3 font-nunito text-[16px] md:text-[18px]'>Start shopping now</button>
+                    <div className='flex w-[200px] md:w-[295px] lg:h-[48px] justify-normal'>
+                        <button className='flex w-full h-full bg-[#E24F29] px-3 text-white rounded-[60px] gap-[10px] py-2 lg:py-3 font-nunito text-[16px] md:text-[18px]'>
+                            <ShoppingCart width={20} />
+                            Start shopping now
+
+                        </button>
                     </div>
                 </Link>
             </div>
@@ -51,15 +56,15 @@ const RightSection = () => {
         <div className='w-full md:w-[45%] lg:w-[30%] flex flex-col md:pt-[50px] px-6 '>
             <div className="flex items-center md:gap-[30px] top-[207px] w-[95%] mx-auto md:w-[308px] h-[200px] md:h-full">
                 <div className='flex w-full md:flex-col gap-y-6 justify-between items-center h-full'>
-                    <Link href="/Lessons" className='hover:scale-105 ease-in-out duration-300 items-center'>
-                        <HeroLink text='Guitar lessons' />
-                    </Link>
-                    <Link href="/" className='hover:scale-105 ease-in-out duration-300'>
-                        <HeroLink text='Guitar' />
-                    </Link>
-                    <Link href="/" className='hover:scale-105 ease-in-out duration-300'>
-                        <HeroLink text='Accessories' />
-                    </Link>
+                    <div className='hover:scale-105 ease-in-out duration-300 items-center'>
+                        <HeroText text='Guitar lessons' />
+                    </div>
+                    <div className='hover:scale-105 ease-in-out duration-300'>
+                        <HeroText text='Guitar' />
+                    </div>
+                    <div className='hover:scale-105 ease-in-out duration-300'>
+                        <HeroText text='Accessories' />
+                    </div>
                 </div>
             </div>
         </div>
