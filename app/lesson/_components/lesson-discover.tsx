ceeca746a8a1733340cf02f7ba94b431195097d7
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-10-20 13:52:28 +0100

    lesson discover fixed

--- app/lesson/_components/lesson-discover.tsx
+++ app/lesson/_components/lesson-discover.tsx
@@ -7,14 +7,19 @@ const LessonDiscover = () => {
     return (
 
         <>
-            <div className="flex items-center justify-between mt-4 md:mt-[60px] ">
-                <div className="md:w-[557px] ">
-                    <p className="font-nunito font-light text-[64px] leading-[67px] p-3 ">Discover the Joy of  <span className="text-[#DF3B11] font-medium ">Playing Guitar</span></p>
+            <div className="flex flex-col md:flex-row items-center justify-between mt-4 md:mt-[60px] space-y-6 md:space-y-0">
+                <div className="md:w-[557px]">
+                    <p className="font-nunito font-light text-[36px] md:text-[64px] leading-[40px] md:leading-[67px] p-3 text-center md:text-left">
+                        Discover the Joy of <span className="text-[#DF3B11] font-medium">Playing Guitar</span>
+                    </p>
                 </div>
-                <div className="md:w-[588px] ">
-                    <p className="font-hankenGrotesk font-normal text-[24px] leading-[30px] p-3 "> Our courses are designed to fit your schedule and budget, so you can learn at your own pace and on your own terms.</p>
+                <div className="md:w-[588px]">
+                    <p className="font-hankenGrotesk font-normal text-[18px] md:text-[24px] leading-[26px] md:leading-[30px] p-3 text-center md:text-left">
+                        Our courses are designed to fit your schedule and budget, so you can learn at your own pace and on your own terms.
+                    </p>
                 </div>
             </div>
+
 
             <div className='w-full mx-auto mt-10 md:mt-[75px] justify-center  '>
                 <div className=' mx-auto flex flex-col md:flex-row  justify-between gap-8'>
